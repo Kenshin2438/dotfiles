cfd99eb491640aa6cc3c22aedca58d93fd39a89a
Kenshin2438 <2953957899@qq.com>
2025-06-16 02:00:50 +0800

    Fix(proxy)
    - Used `ValidateSet` to limit function parameters for `Enable-Proxy`.
    - Added `NO_PROXY` environment variable to support some specific applications.
    
    Signed-off-by: Kenshin2438 <2953957899@qq.com>

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,17 +8,21 @@ Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 ## Proxy
-Function Enable-Proxy([int] $port) {
+Function Enable-Proxy {
+    Param([ValidateSet(7897, 2080)] $port)
+
     [String] $proxy = "http://127.0.0.1:${port}"
     Write-Output "`e[2m[Proxy ON] ALL_PROXY, HTTPS_PROXY and HTTP_PROXY set to $proxy.`e[0m"
     Set-Item env:ALL_PROXY $proxy
     Set-Item env:HTTP_PROXY $proxy
     Set-Item env:HTTPS_PROXY $proxy
+    Set-Item env:NO_PROXY "localhost,127.0.0.1,::1,192.168.0.0/16"
 }
 Function Disable-Proxy {
     if ($env:ALL_PROXY) { Remove-Item env:ALL_PROXY }
     if ($env:HTTP_PROXY) { Remove-Item env:HTTP_PROXY }
     if ($env:HTTPS_PROXY) { Remove-Item env:HTTPS_PROXY }
+    if ($env:NO_PROXY) { Remove-Item env:NO_PROXY }
     Write-Output "`e[2m[Proxy OFF] unset ALL_PROXY, HTTPS_PROXY and HTTP_PROXY.`e[0m"
 }
 
