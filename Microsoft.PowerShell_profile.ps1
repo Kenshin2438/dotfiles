550989582900dc2222efaeabb008ca07d45fa7ac
Kenshin2438 <2953957899@qq.com>
2025-07-11 17:10:35 +0800

    cleanup: nekoray & CommandNotFound

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,7 +9,7 @@ Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 ## Proxy
 Function Enable-Proxy {
-    Param([ValidateSet(7897, 2080)] $port)
+    Param([ValidateSet(7897)] $port)
 
     [String] $proxy = "http://127.0.0.1:${port}"
     Write-Output "`e[2m[Proxy ON] ALL_PROXY, HTTPS_PROXY and HTTP_PROXY set to $proxy.`e[0m"
@@ -25,9 +25,6 @@ Function Disable-Proxy {
     if ($env:NO_PROXY) { Remove-Item env:NO_PROXY }
     Write-Output "`e[2m[Proxy OFF] unset ALL_PROXY, HTTPS_PROXY and HTTP_PROXY.`e[0m"
 }
-
-## PowerToys CommandNotFound suggestions
-Import-Module -Name Microsoft.WinGet.CommandNotFound
 
 ## Fast scoop search drop-in replacement
 Invoke-Expression (&scoop-search --hook)
